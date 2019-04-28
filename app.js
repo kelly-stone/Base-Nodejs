2b24d9ba43d3dbaa311188b78c5599abbc0fcc4c
Kelly <kellyandstone@gmail.com>
2019-04-28 20:59:28 +1200

    basic reviewing

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("hello world");
+// setTimeout(() => {
+//   console.log("3 sec passed!");
+// }, 3000);
+
+// setInterval(() =>{
+//     console.log("every 2 sec, stopping by ctr c");
+// },2000)
+
+// var time = 0;
+// setInterval(() => {
+//   time += 2;
+//   console.log(time, " sec passed! ");
+// }, 2000);
+
+// var time = 0;
+// var timer = setInterval(() => {
+//   time += 2;
+//   console.log(time, "sec have passed, but stop when time >5");
+//   if (time > 5) {
+//     clearInterval(timer);
+//   }
+// }, 2000);
+
+//console.log(__dirname); //shows location
+//console.log(__filename); //shows file location
+
+//more.. require export
