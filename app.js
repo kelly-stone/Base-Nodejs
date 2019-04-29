540fad8cf7f6f11caa93349305cc535c5b9774f7
Kelly <kellyandstone@gmail.com>
2019-04-29 14:02:05 +1200

    require, exports

--- app.js
+++ app.js
@@ -1,37 +1,3 @@
-// setTimeout(() => {
-//   console.log("3 sec passed!");
-// }, 3000);
+var counter = require("./count");
 
-// setInterval(() =>{
-//     console.log("every 2 sec, stopping by ctr c");
-// },2000)
-
-// var time = 0;
-// setInterval(() => {
-//   time += 2;
-//   console.log(time, " sec passed! ");
-// }, 2000);
-
-// var time = 0;
-// var timer = setInterval(() => {
-//   time += 2;
-//   console.log(time, "sec have passed, but stop when time >5");
-//   if (time > 5) {
-//     clearInterval(timer);
-//   }
-// }, 2000);
-
-//console.log(__dirname); //shows location
-//console.log(__filename); //shows file location
-
-// function callFunction(fun, name) {
-//   fun(name);
-// }
-// var sayBye = function(name) {
-//   console.log("Bye " + name);
-// };
-// callFunction(sayBye, "Kelly");
-
-// callFunction(function(name) {
-//     console.log("Bye " + name);
-//     }, "Kelly")
+console.log(counter(["ruby", "nodejs", "react"]));
---
+++ count.js
@@ -0,0 +1,4 @@
+var counter = function(arr) {
+  return "there are " + arr.length + " element in the array";
+};
+module.exports = counter;
