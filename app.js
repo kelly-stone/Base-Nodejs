a0d8ce03bfe7e2e0dd87a6495ac2d366ef58de66
Kelly <kellyandstone@gmail.com>
2019-04-30 13:21:36 +1200

    fs.readFileSync and fs.writeFileSync

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-var all = require("./count");
+// var all = require("./count");
 
-console.log(all.counter(["ruby", "nodejs", "react"]));
-console.log(all.adder(2, 3));
-console.log(all.pi);
+// console.log(all.counter(["ruby", "nodejs", "react"]));
+// console.log(all.adder(2, 3));
+// console.log(all.pi);
+
+var fs = require("fs");
+var readMe = fs.readFileSync("readMe.txt", "utf8");
+fs.writeFileSync("writeMe.txt", "Hello world");
