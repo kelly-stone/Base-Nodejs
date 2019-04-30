da5d85bc43af1e2b7f1855d85e247cc3da1c3e2a
Kelly <kellyandstone@gmail.com>
2019-05-01 09:26:16 +1200

    reviewing: fs.readFile/writeFile/unlink/mkdir/rmdir or sync

--- app.js
+++ app.js
@@ -5,5 +5,18 @@
 // console.log(all.pi);
 
 var fs = require("fs");
-var readMe = fs.readFileSync("readMe.txt", "utf8");
-fs.writeFileSync("writeMe.txt", "Hello world");
+// var readMe = fs.readFileSync("readMe.txt", "utf8");
+// //console.log(readMe);
+// fs.writeFileSync("writeMe.txt", "Hello world");
+
+// fs.unlink("writeMe.txt", function() {
+//   console.log("remove writeMe");
+// });
+
+fs.mkdir("stuff", function() {
+  fs.readFile("readMe.txt", "utf8", function(err, data) {
+    fs.writeFile("./stuff/writeMe.txt", data, function() {
+      console.log("create stuff with readMe data");
+    });
+  });
+});
