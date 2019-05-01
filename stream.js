6ba16fd4bfa2c822044be4821e7e6a229174a207
Kelly <kellyandstone@gmail.com>
2019-05-01 13:00:50 +1200

    there is a simple way to copy data by using pipe()

--- stream.js
+++ stream.js
@@ -1,5 +1,6 @@
 var fs = require("fs");
 
+//read:
 var myReadSteam = fs.createReadStream(__dirname + "/readMe.txt", "utf8");
 var data = "";
 myReadSteam.on("data", function(chunk) {
@@ -9,6 +10,7 @@ myReadSteam.on("end", function() {
   console.log(data);
 });
 
+//write:
 var myWriteSteam = fs.createWriteStream(__dirname + "/writeMe.txt", "utf8");
 var data = "";
 myReadSteam.on("data", function(chunk) {
@@ -17,3 +19,15 @@ myReadSteam.on("data", function(chunk) {
 myReadSteam.on("end", function() {
   //   console.log(data);
 });
+
+//change data in writeMe.txt:
+// var myWriteSteam = fs.createWriteStream(__dirname + "/writeMe.txt", "utf8");
+// var writeData = "Helllllllllllooooooo";
+// myWriteSteam.write(writeData);
+// myWriteSteam.end();
+
+//there is a simple way to copy data by using pipe():
+// var fs = require("fs");
+// var myReadSteam = fs.createReadStream(__dirname + "/readMe.txt", "utf8");
+// var myWriteSteam = fs.createWriteStream(__dirname + "/writeMe.txt", "utf8");
+// myReadSteam.pipe(myWriteSteam);
