66c2a1549a3661d5830a707f3573d93cf5840ef0
Kelly <kellyandstone@gmail.com>
2019-05-01 12:41:58 +1200

    stream createWriteStream

--- stream.js
+++ stream.js
@@ -1,12 +1,19 @@
 var fs = require("fs");
 
 var myReadSteam = fs.createReadStream(__dirname + "/readMe.txt", "utf8");
-
 var data = "";
-
 myReadSteam.on("data", function(chunk) {
   data += chunk;
 });
 myReadSteam.on("end", function() {
   console.log(data);
+});
+
+var myWriteSteam = fs.createWriteStream(__dirname + "/writeMe.txt", "utf8");
+var data = "";
+myReadSteam.on("data", function(chunk) {
+  myWriteSteam.write(chunk);
+});
+myReadSteam.on("end", function() {
+  //   console.log(data);
 });
