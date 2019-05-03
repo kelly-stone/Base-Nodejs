b58133e6dd9e4ddec4a01a8aef32297f5c8323f9
Kelly <kellyandstone@gmail.com>
2019-05-03 13:50:49 +1200

    reviewing content-type:application/json

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ var http = require("http");
 
 var onRequest = function(request, response) {
   console.log("Request received");
-  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.writeHead(200, { "Content-Type": "application/json" });
   // response.write('Hello from out application');
-  response.end("Hello from our applications");
+  var myObj = {
+    name: "kelly",
+    job: "programmer",
+    age: 27
+  };
+  response.end(JSON.stringify(myObj));
 };
 
 var server = http.createServer(onRequest);
 
-server.listen(3000);
-console.log("Server started on localhost port 3000");
+server.listen(8000);
+console.log("Server started on localhost port 8000");
