26c3daad56a5c4e120fe7aee3a51233e2a82dc81
Kelly <kellyandstone@gmail.com>
2019-05-03 15:31:08 +1200

    using require and export on server

--- server.js
+++ server.js
@@ -16,15 +16,19 @@
 var http = require("http");
 var fs = require("fs");
 
-var onRequest = function(request, response) {
-  console.log("Request received");
-  response.writeHead(200, { "Content-Type": "text/html" });
-  var myReadStream = fs.createReadStream(__dirname + "/index.html", "utf8");
-  // response.write('Hello from out application');
-  myReadStream.pipe(response);
-};
+function startServer() {
+  var onRequest = function(request, response) {
+    console.log("Request received");
+    response.writeHead(200, { "Content-Type": "text/html" });
+    var myReadStream = fs.createReadStream(__dirname + "/index.html", "utf8");
+    // response.write('Hello from out application');
+    myReadStream.pipe(response);
+  };
 
-var server = http.createServer(onRequest);
+  var server = http.createServer(onRequest);
 
-server.listen(3000);
-console.log("Server started on localhost port 3000");
+  server.listen(3000, "127.0.0.1");
+  console.log("Server started on localhost port 3000");
+}
+
+exports.startServer = startServer;
