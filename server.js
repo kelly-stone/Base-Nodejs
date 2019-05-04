f9e73678060fc9a682c77af38c6794e2104cdb77
Kelly <kellyandstone@gmail.com>
2019-05-04 20:52:49 +1200

    reviewing request.url with html, json, and 404

--- server.js
+++ server.js
@@ -18,17 +18,36 @@ var fs = require("fs");
 
 function startServer() {
   var onRequest = function(request, response) {
-    console.log("Request received");
-    response.writeHead(200, { "Content-Type": "text/html" });
-    var myReadStream = fs.createReadStream(__dirname + "/index.html", "utf8");
-    // response.write('Hello from out application');
-    myReadStream.pipe(response);
+    console.log("request received" + request.url);
+    if (request.url === "/" || request.url === "/home") {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      var myReadStream = fs.createReadStream(__dirname + "/index.html", "utf8");
+      // response.write('Hello from out application');
+      myReadStream.pipe(response);
+    } else if (request.url === "/review") {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      var myReadStream = fs.createReadStream(
+        __dirname + "/review.html",
+        "utf8"
+      );
+      // response.write('Hello from out application');
+      myReadStream.pipe(response);
+    } else if (request.url === "/api/v1/records") {
+      response.writeHead(200, { "Content-Type": "application/json" });
+      var jsonObj = {
+        name: "kelly"
+      };
+      response.end(JSON.stringify(jsonObj));
+    } else {
+      response.writeHead(404, { "Content-Type": "text/html" });
+      fs.createReadStream(__dirname + "/404.html", "utf8").pipe(response);
+    }
   };
 
   var server = http.createServer(onRequest);
 
-  server.listen(3000, "127.0.0.1");
-  console.log("Server started on localhost port 3000");
+  server.listen(4000);
+  console.log("Server started on localhost port 1000");
 }
 
 exports.startServer = startServer;
